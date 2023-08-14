a0ed3f4a1113b6631d291d8dd8dfd8a9a7a8fe10
floer <florian.sennhauser@bluewin.ch>
2023-08-14 18:45:20 +0200

    Adding features to helloworld_webserver.js

--- helloworld_webserver.js
+++ helloworld_webserver.js
@@ -5,6 +5,11 @@ app.get('/hello', (req, res) => {
   res.send('Hallo Welt');
 });
 
+app.get('/hello/:myName', (req, res) => {
+  const name = req.params.myName;
+  res.send(`Hallo ${name}`);
+});
+
 app.listen(3000, () => {
   console.log('Der Webserver ist auf Port 3000 gestartet!');
 });
